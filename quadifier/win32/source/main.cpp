f4e78e091c845f6fd6411153dd06615c18003d10
Peter LaValle <peter.lavalle@gmail.com>
2015-03-20 17:07:40 +0000

    .

--- quadifier/win32/source/main.cpp
+++ quadifier/win32/source/main.cpp
@@ -35,29 +35,29 @@ using namespace std;
 // workaround for issue seen on Windows 7 with NVIDIA 
 void triggerStereo()
 {
-    // attributes for an OpenGL stereo window
-    GLWindow::Attributes attributes;
-    attributes[WGL_STEREO_ARB] = GL_TRUE;
+	// attributes for an OpenGL stereo window
+	GLWindow::Attributes attributes;
+	attributes[WGL_STEREO_ARB] = GL_TRUE;
 
-    // create and destroy a small stereo window: this is here to work around
-    // an issue with windows failing to switch to quad-buffer mode (although
-    // the window is successfully created and reports a stereo pixel format)
-    // note: the window is invisible (not shown) but typically causes the
-    // windows desktop to flash briefly (first run only) as stereo activates
-    GLWindow window;
-    window.create( 0, "", 0, 0, 0, 8, 8, 0, 0, DefWindowProc, 0, attributes );
-    window.destroy();
+	// create and destroy a small stereo window: this is here to work around
+	// an issue with windows failing to switch to quad-buffer mode (although
+	// the window is successfully created and reports a stereo pixel format)
+	// note: the window is invisible (not shown) but typically causes the
+	// windows desktop to flash briefly (first run only) as stereo activates
+	GLWindow window;
+	window.create(0, "", 0, 0, 0, 8, 8, 0, 0, DefWindowProc, 0, attributes);
+	window.destroy();
 }
 
 //-----------------------------------------------------------------------------
 
-int main ( int argc, char **argv )
+int main(int argc, char **argv)
 {
-    // trigger stereo mode in Windows by creating a temporary stereo window
-    triggerStereo();
+	// trigger stereo mode in Windows by creating a temporary stereo window
+	triggerStereo();
 
-    // the name of our DLL to be injected
-    static const string moduleName( "module.dll" );
+	// the name of our DLL to be injected
+	static const string moduleName("module.dll");
 
     // have we got an application filename?
     if ( argc > 1 ) {
@@ -71,19 +71,32 @@ int main ( int argc, char **argv )
             commandLine += string(argv[i]);
         }
 
-        // attempt to launch the application and inject the DLL
-        if ( !hive::createProcessWithDLL(
-            applicationName,    // name of executable
-            moduleName,         // name of DLL
-            commandLine         // command line options
-        ) )
-            cerr << "error: failed to start " << applicationName << endl;
-    } else {
-        // display usage instructions
-        cerr << "usage: launcher <executable> [options]\n";
-    }
+	// have we got an application filename?
+	if (argc > 1) {
+		// name of the application executable
+		string applicationName(argv[1]);
 
-    return 0;
+		// build command line string
+		string commandLine;
+		for (int i = 2; i < argc; ++i) {
+			if (i > 2) commandLine += " ";
+			commandLine += string(argv[i]);
+		}
+
+		// attempt to launch the application and inject the DLL
+		if (!hive::createProcessWithDLL(
+			applicationName,    // name of executable
+			moduleName,         // name of DLL
+			commandLine         // command line options
+			))
+			cerr << "error: failed to start " << applicationName << endl;
+	}
+	else {
+		// display usage instructions
+		cerr << "usage: launcher <executable> [options]\n";
+	}
+
+	return 0;
 }
 
 //-----------------------------------------------------------------------------
